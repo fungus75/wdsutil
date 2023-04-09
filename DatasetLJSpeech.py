3623b96fd6d1b488289161d2b5402387a5319769
fungus75 <rene@pilz.cc>
2023-04-09 21:58:43 +0200

    Something in between: non-working-version but dayly commit

--- DatasetLJSpeech.py
+++ DatasetLJSpeech.py
@@ -7,7 +7,16 @@ from DatasetBase import DatasetBase
 class DatasetLJSpeech(DatasetBase):
 
     def export_dataset(self, path=None):
-        pass
+        if path is None:
+            sys.exit("Error: You must provide a valid path for exporting dataset.")
+        if os.path.exists(path):
+            sys.exit("Error: Export path exist, please provide an non-existing path!")
+
+        basisPathWaveFile = self._eval_wave_path(path)
+        metafile = open(path, 'w')
+        for i in range(0, self.content.size()):
+            dataset = self.content.get(i)
+            os.
 
     def get_readable_name(self):
         return 'ljspeech'
@@ -18,11 +27,14 @@ class DatasetLJSpeech(DatasetBase):
         print(" See https://keithito.com/LJ-Speech-Dataset/ for a description of the type")
         print(" You must provide the full path to the metadata-file as additional file parameter (-if or -of)")
 
+    def _eval_wave_path(self, path=None):
+        return os.path.join(os.path.dirname(path), "wavs")
+
     def import_dataset(self, path=None):
         if path is None or not os.path.exists(path):
             sys.exit("Error: You must provide a valid path for dataset.")
 
-        basisPathWaveFile = os.path.join(os.path.dirname(path),"wavs")
+        basisPathWaveFile = self._eval_wave_path(path)
         metafile = open(path,'r')
         for line in metafile:
             parts = line.split('|')
